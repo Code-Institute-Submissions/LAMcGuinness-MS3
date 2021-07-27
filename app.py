7f24f7505b966bb0049f3387daa0da908f079adc
LAMcGuinness <laumcguinness@gmail.com>
2021-07-27 18:52:36 +0000

    created the Flask App

--- app.py
+++ app.py
@@ -17,14 +17,14 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+
 @app.route("/")
-@app.route("/get_tasks")
-def get_tasks():
-    tasks = mongo.db.tasks.find()
-    return render_template("tasks.html", tasks=tasks)
+def hello():
+    return "hello world!.. again!"
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+            
